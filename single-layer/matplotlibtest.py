02741ec6bf5168cc4fe52997573d3fb198c5cae0
jj8724 <66822602+jj8724@users.noreply.github.com>
2020-06-12 11:48:09 +0100

    Create matplotlibtest.py

---
+++ single-layer/matplotlibtest.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#dat=(0,1)
+plt.plot(0,1)
+#plt.ion()
+plt.show()    
+for i in range (18):
+#    dat.append(random.uniform(0,1))
+    plt.plot(0,i)
+    plt.show()
+#    time.sleep(1)
